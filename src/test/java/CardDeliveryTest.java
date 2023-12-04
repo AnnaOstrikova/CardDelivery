cf66959cedb357faddaa0818c0db993fd28b7113
Anna <pomozovaanna@yandex.ru>
2023-12-04 16:04:46 +0500

    Commit test

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldDeliveryCard() {
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Казань");
+        String planningData = generateDate(6, "dd.MM.yyyy");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(planningData);
+        $("[name='name']").setValue("Петр Иванов");
+        $("[name='phone']").setValue("+79990000000");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
